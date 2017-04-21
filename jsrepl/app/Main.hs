8cdb2b13cf39dfada7eefc4ee0c161442383d785
David Raymond Christiansen <david@davidchristiansen.dk>
2017-04-21 12:58:19 -0400

    Reader

--- jsrepl/app/Main.hs
+++ jsrepl/app/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Main (main) where
 
@@ -14,12 +15,18 @@ import Control.Monad.Writer
 import Control.Monad.State
 import Control.Monad.RWS
 
+import Control.Concurrent (threadDelay, forkIO)
+
+import Data.Char (isSpace, isAlpha)
 import Data.List (intersperse)
 
+import qualified Data.Map.Strict as M
+import Data.IORef (IORef(..), newIORef, readIORef, atomicModifyIORef')
+import System.IO.Unsafe (unsafePerformIO) -- yolo
+
 import Control.Monad (guard)
---import Control.Monad.Reader (ReaderT(..), ask, runReader)
 import Control.Monad.IO.Class (MonadIO(..))
-import GHCJS.DOM (currentDocument)
+import GHCJS.DOM (currentDocument, currentWindow)
 import GHCJS.DOM.Document (createElementUnsafe, createTextNodeUnsafe, getBody)
 import GHCJS.DOM.EventM (EventM, event, on, uiWhich)
 import GHCJS.DOM.HTMLInputElement (getValueUnsafe, setValue, setType)
@@ -34,8 +41,9 @@ import GHCJS.DOM.Types ( Document
                        , IsNode
                        , Text
                        , unsafeCastTo)
+--import GHCJS.DOM.WindowTimers (setTimeout)
 
-import qualified GHCJS.DOM.Element as E (click, getAttribute, keyPress, setAttribute)
+import qualified GHCJS.DOM.Element as E (click, getAttribute, keyPress, setAttribute, load, getOffsetWidth)
 
 ---
 
@@ -44,22 +52,29 @@ import qualified Data.Text as T
 
 import Pretty
 
+
+----------------
+
+-----------------------------------
 -- Widths in HTML-land will be ints representing pixels
 
-renderAtom :: (IsDocument d) => d -> Atom Int -> IO HTMLElement
+preProcess :: T.Text -> T.Text
+preProcess = T.replace (T.pack " ") (T.pack " ") -- it's a non breaking space char
+
+renderAtom :: (IsDocument d) => d -> Atom Double -> IO HTMLElement
 renderAtom htmlDoc ANewline = createElement htmlDoc "br" HTMLElement
-renderAtom htmlDoc (AChunk (CText t)) = do
+renderAtom htmlDoc (AChunk (CText (preProcess -> t))) = do
   txt <- createTextNodeUnsafe' htmlDoc (T.unpack t)
   elt <- createElement htmlDoc "span" HTMLElement
   appendChild elt (Just txt)
   return elt
 renderAtom htmlDoc (AChunk (CSpace w)) = do
   elt <- createElement htmlDoc "span" HTMLElement
-  E.setAttribute elt "style" ("width: " ++ show w ++ "px")
+  E.setAttribute elt "style" ("display: inline-block; min-width: " ++ show (round (w * spaceWidth)) ++ "px;")
   return elt
 
 
-render :: (IsDocument d) => d -> HTMLElement -> (ann -> String) -> POut Int ann -> IO ()
+render :: (IsDocument d) => d -> HTMLElement -> (ann -> String) -> POut Double ann -> IO ()
 render htmlDoc parent toClass out = go out >> return ()
   where
     go out =
@@ -88,11 +103,13 @@ toR (Cons e1 e2) =
     ProperList es -> ProperList (toR e1 : es)
     ImproperList es -> ImproperList (toR e1 : es)
 
-e1,e2,e3,e4 :: Expr
+e1,e2,e3,e4,e5 :: Expr
 e1 = Cons (Symbol (T.pack "quote")) (Cons (Symbol (T.pack "hello")) Nil)
 e2 = Cons (Symbol (T.pack "λ")) (Cons (Cons (Symbol (T.pack ("x"))) Nil) (Cons (Symbol (T.pack "x")) Nil))
 e3 = foldr Cons Nil (replicate 30 (Symbol (T.pack "i")))
 e4 = foldr Cons Nil (replicate 30 (Symbol (T.pack "M")))
+e5 = let go n = foldr Cons Nil (replicate n (Symbol (T.pack (show n))))
+     in foldr Cons Nil [go n | n <- [0 .. 30]]
 
 ----------------------
 
@@ -124,43 +141,56 @@ askDoc = RenderM  (pure . fst)
 askParent :: RenderM HTMLElement
 askParent = RenderM (pure . snd)
 
-measureChunk :: (Chunk Int, [String]) -> RenderM Int
-measureChunk (CText txt, fmt) = do
-  doc    <- askDoc
-  parent <- askParent
-  elt <- createElement doc "span" HTMLElement
-  txtNode <- createTextNodeUnsafe' doc (T.unpack txt)
-  appendChild elt (Just txtNode)
-  E.setAttribute elt "className" (concat (intersperse " " fmt))
-  E.setAttribute elt "display" "hidden"
-  appendChild parent (Just elt)
-  --Just w <- E.getAttribute elt "offsetWidth"
-  let w = length (T.unpack txt) -- FIXME
-  removeChild parent (Just elt)
-  return w --(read w)
+chunkWidths :: IORef (M.Map (Chunk Double, [String]) Double)
+chunkWidths = unsafePerformIO (newIORef M.empty) -- TODO: move into RenderM state
+
+spaceWidth :: Double
+spaceWidth = 4
+
+measureChunk :: (Chunk Double, [String]) -> RenderM Double
+measureChunk ch@(CText (preProcess -> txt), fmt) = do
+  found <- M.lookup ch <$> liftIO (readIORef chunkWidths)
+  case found of
+    Just r -> return r
+    Nothing -> do
+      doc    <- askDoc
+      parent <- askParent
+
+      elt <- createElement doc "span" HTMLElement
+      txtNode <- createTextNodeUnsafe' doc (T.unpack txt)
+      appendChild elt (Just txtNode)
+      E.setAttribute elt "className" (concat (intersperse " " fmt))
+      E.setAttribute elt "display" "hidden"
+      appendChild parent (Just elt)
+      liftIO $ threadDelay 1
+      w <- E.getOffsetWidth elt
+      removeChild parent (Just elt)
+
+      liftIO $ atomicModifyIORef' chunkWidths $ \ old -> (M.insert ch w old, ())
+      return (w / spaceWidth) -- HACK ALERT - in the future, actually measure spaces
 measureChunk (CSpace w, _)  = pure w
 
-newtype DocM a = DocM { unDocM :: RWST (PEnv Int LispAnn [String])
-                                       (POut Int LispAnn)
-                                       (PState Int [String])
+newtype DocM a = DocM { unDocM :: RWST (PEnv Double LispAnn [String])
+                                       (POut Double LispAnn)
+                                       (PState Double [String])
                                        (MaybeT RenderM)
                                         a }
   deriving
     ( Functor, Applicative, Monad, Alternative
-    , MonadReader (PEnv Int LispAnn [String])
-    , MonadWriter (POut Int LispAnn)
-    , MonadState (PState Int [String])
+    , MonadReader (PEnv Double LispAnn [String])
+    , MonadWriter (POut Double LispAnn)
+    , MonadState (PState Double [String])
     )
 
-instance MonadPretty Int LispAnn [String] DocM
+instance MonadPretty Double LispAnn [String] DocM
 
-runDocM :: PEnv Int LispAnn [String] -> PState Int [String] -> DocM a
-        -> RenderM (Maybe (PState Int [String], POut Int LispAnn, a))
+runDocM :: PEnv Double LispAnn [String] -> PState Double [String] -> DocM a
+        -> RenderM (Maybe (PState Double [String], POut Double LispAnn, a))
 runDocM e s d = fmap rearrange <$> (runMaybeT (runRWST (unDocM d) e s))
   where rearrange (a, s', o) = (s', o, a)
 
 
-instance Measure Int [String] DocM where
+instance Measure Double [String] DocM where
   measure l = sum <$> mapM (liftRender . measureChunk) l
 
 liftRender :: RenderM a -> DocM a
@@ -171,9 +201,17 @@ prettyR (RSymbol e) = text e
 prettyR (ProperList []) = text (T.pack "'()")
 prettyR (ProperList [RSymbol q, e]) | q == T.pack "quote" =
   text (T.pack "'") >> prettyR e
-prettyR (ProperList xs) =
-  collection (text (T.pack "(")) (text (T.pack ")")) (return ())
-    (map prettyR xs)
+prettyR (ProperList [x]) = do
+  text (T.pack "(")
+  prettyR x
+  text (T.pack ")")
+prettyR (ProperList (x:xs)) = do
+  text (T.pack "(")
+  align $ do prettyR x
+             space 1
+             hvsep (map prettyR xs)
+--  hvsep $ align (prettyR x) : (map prettyR xs)
+  text (T.pack ")")
 prettyR (ImproperList xs) =
   collection (text (T.pack "(")) (text (T.pack ")")) (return ())
     (withDot (map prettyR xs))
@@ -182,22 +220,91 @@ prettyR (ImproperList xs) =
         withDot [x, y] = [x, char '.', y]
         withDot (x:y:zs) = x : withDot (y:zs)
 
-d1, d2, d3, d4 :: DocM ()
+d1, d2, d3, d4, d5 :: DocM ()
 d1 = prettyR (toR e1)
 d2 = prettyR (toR e2)
 d3 = prettyR (toR e3)
 d4 = prettyR (toR e4)
+d5 = prettyR (toR e5)
 --------------------
 
-state0 :: PState Int [String]
+newtype Parse a = Parse { parse :: String -> Either String (String, a) }
+  deriving Functor
+
+instance Applicative Parse where
+  pure x = Parse $ \ txt -> Right (txt, x)
+  (Parse pf) <*> (Parse px) =
+    Parse $ \txt ->
+      case pf txt of
+        Left err -> Left err
+        Right (rest, f) ->
+          case px rest of
+            Left err -> Left err
+            Right (rest', x) -> Right (rest', f x)
+
+instance Alternative Parse where
+  empty = Parse $ \ xs -> Left $ "Failed to parse " ++ show xs
+  p1 <|> p2 =
+    Parse $ \ txt ->
+      case parse p1 txt of
+        Left err -> parse p2 txt
+        Right x -> Right x
+
+token :: Parse a -> Parse a
+token p = Parse $ \s ->
+  case parse p s of
+    Left err -> Left err
+    Right (rest, res) -> Right (dropWhile isSpace rest, res)
+
+readSym :: Parse Expr
+readSym = Parse $ \ txt ->
+  case takeWhile isAlpha txt of
+    [] -> Left $ "Not a symbol at " ++ take 20 txt
+    ok@(c:cs) -> Right (drop (length ok) txt, Symbol (T.pack ok))
+
+readQuoted = Parse $ \ txt ->
+  case txt of
+    ('\'':cs) ->
+      fmap (\(r, e) -> (r, (Cons (Symbol (T.pack "quote")) (Cons e Nil))))
+           (parse readExpr cs)
+    other ->
+      Left $ "Expected quote at: " ++ other
+
+readChar :: Char -> Parse ()
+readChar c = Parse $ \txt ->
+  case txt of
+    [] -> Left $ "Expected " ++ show c
+    (x:xs) ->
+      if c == x
+        then Right (xs, ())
+        else Left $ "Expected " ++ show c ++ " got " ++ show x
+
+list :: Parse Expr
+list = foldr Cons Nil <$>
+         (token (readChar '(') *>
+          many readExpr <*
+          token (readChar ')'))
+
+phrase :: Parse a -> Parse a
+phrase p = Parse $ \ txt ->
+  case parse p txt of
+    Left e -> Left e
+    Right ([], x) -> Right ([], x)
+    Right (xs, x) -> Left $ "Expected to consume all input, but left over: " ++
+                            show (take 15 xs)
+
+readExpr = token readSym <|> readQuoted <|> list
+
+--------------------
+state0 :: PState Double [String]
 state0 = PState
   { curLine = []
   }
 
-env0 :: Monoid fmt => Int ->  PEnv Int a fmt
+env0 :: Monoid fmt => Double ->  PEnv Double a fmt
 env0 w = PEnv
   { maxWidth = w
-  , maxRibbon = (w * 4) `div` 5
+  , maxRibbon = (w * 4) / 5
   , layout = Break
   , failure = CantFail
   , nesting = 0
@@ -206,10 +313,10 @@ env0 w = PEnv
   }
 
 
-execDoc :: Document -> HTMLElement -> DocM () -> IO (POut Int LispAnn)
+execDoc :: (MonadIO m) => Document -> HTMLElement -> DocM () -> m (POut Double LispAnn)
 execDoc htmlDoc parent d = do
-  (Just parentWidth) <- E.getAttribute parent "offsetWidth"
-  foo <- runRender (runDocM (env0 (read parentWidth)) state0 d) (htmlDoc, parent)
+  parentWidth <- E.getOffsetWidth parent
+  foo <- liftIO $ runRender (runDocM (env0 parentWidth) state0 d) (htmlDoc, parent)
   case foo of
     Nothing -> pure $ PAtom $ AChunk $ CText $ T.pack "<internal pretty printing error>"
     Just (_, o, ()) -> pure o
@@ -218,16 +325,24 @@ execDoc htmlDoc parent d = do
 createElement doc tagName tagType =
   createElementUnsafe doc (Just tagName) >>= unsafeCastTo tagType
 
+record :: Document -> HTMLElement -> String -> EventM e t ()
 record doc parent line = do
-  textNode <- createTextNodeUnsafe' doc line
-  br       <- createElement doc "br" HTMLBRElement
-  appendChild parent (Just textNode)
-  appendChild parent (Just br)
-  return ()
+  case parse (phrase readExpr) line of
+    Left err -> liftIO $ putStrLn err
+    Right (_, e) ->
+      do let d = prettyR (toR e)
+         o <- liftIO $ execDoc doc parent d
+         liftIO $ render doc parent (const "") o
+         br <- createElement doc "br" HTMLBRElement
+         appendChild parent (Just br)
+         liftIO $ threadDelay 1
+         return ()
+
 
 main :: IO ()
 main = do
   Just doc <- currentDocument
+  Just window <- currentWindow
   Just body <- getBody doc
 
   transcript <- createElement doc "div" HTMLElement
@@ -235,27 +350,39 @@ main = do
   button     <- createElement doc "input" HTMLInputElement
   setType button "button"
 
-
   appendChild body (Just transcript)
-
-  let terpri = createElement doc "br" HTMLBRElement >>= appendChild transcript . Just
-
-  o1 <- execDoc doc transcript d1
-  render doc transcript (const "") o1
-  terpri
-  o2 <- execDoc doc transcript d2
-  render doc transcript (const "") o2
-  terpri
-  o3 <- execDoc doc transcript d3
-  render doc transcript (const "") o3
-  terpri
-  o4 <- execDoc doc transcript d4
-  render doc transcript (const "") o4
-  terpri
-
-
+  threadDelay 1
   appendChild body (Just input)
+  threadDelay 1
   appendChild body (Just button)
+  threadDelay 1
+
+  forkIO $ do
+    threadDelay 1
+    putStrLn "hello"
+    out <- E.getOffsetWidth transcript :: IO (Double)
+    putStrLn (show out)
+
+    let terpri = (createElement doc "br" HTMLBRElement >>= appendChild transcript . Just) >>
+                 pure ()
+
+    o1 <- liftIO $ execDoc doc transcript d1
+    liftIO $ render doc transcript (const "") o1
+    terpri
+    o2 <- liftIO $ execDoc doc transcript d2
+    liftIO $ render doc transcript (const "") o2
+    terpri
+    -- o3 <- execDoc doc transcript d3
+    -- render doc transcript (const "") o3
+    -- terpri
+    -- o4 <- execDoc doc transcript d4
+    -- render doc transcript (const "") o4
+    -- terpri
+    o5 <- execDoc doc transcript d5
+    render doc transcript (const "") o5
+    terpri
+    return ()
+
 
   let save line =
         (record doc transcript line >>
