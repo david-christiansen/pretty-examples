bbae5eb43f87439986842ed6cb5debbb5f74ec4d
David Raymond Christiansen <david@davidchristiansen.dk>
2017-04-24 15:35:44 -0400

    Begin splitting the Lisp stuff out

--- jsrepl/app/Main.hs
+++ jsrepl/app/Main.hs
@@ -26,7 +26,7 @@ import Data.List (intersperse)
 
 import qualified Data.Map.Strict as M
 import Data.IORef (IORef(..), newIORef, readIORef, atomicModifyIORef')
-import System.IO.Unsafe (unsafePerformIO) -- yolo
+
 
 import Control.Monad (guard)
 import Control.Monad.IO.Class (MonadIO(..))
@@ -57,137 +57,14 @@ import qualified Data.Text as T
 ---
 
 import Lisp
-
+import Lib
 import Text.PrettyPrint.Final
-
-
-----------------
-
------------------------------------
--- Widths in HTML-land will be doubles representing pixels
-
-preProcess :: T.Text -> T.Text
-preProcess = T.replace (T.pack " ") (T.pack " ") -- it's a non breaking space char
-
-renderAtom :: (IsDocument d) => d -> Atom Double -> IO HTMLElement
-renderAtom htmlDoc ANewline = createElement htmlDoc "br" HTMLElement
-renderAtom htmlDoc (AChunk (CText (preProcess -> t))) = do
-  txt <- createTextNodeUnsafe' htmlDoc (T.unpack t)
-  elt <- createElement htmlDoc "span" HTMLElement
-  appendChild elt (Just txt)
-  return elt
-renderAtom htmlDoc (AChunk (CSpace w)) = do
-  elt <- createElement htmlDoc "span" HTMLElement
-  E.setAttribute elt "style" $
-    "display: inline-block;" ++
-    "min-width: " ++ show (round w) ++ "px;" ++
-    "max-width: " ++ show (round w) ++ "px;"
-  txt <- createTextNodeUnsafe' htmlDoc " "
-  appendChild elt (Just txt)
-  return elt
-
-
-render :: (IsDocument d) => d -> HTMLElement -> (ann -> String) -> POut Double ann -> IO ()
-render htmlDoc parent toClass out = go out >> return ()
-  where
-    go out =
-      case out of
-        PNull -> createTextNodeUnsafe' htmlDoc "" >>= appendChild parent . Just
-        PAtom a -> renderAtom htmlDoc a >>= appendChild parent . Just
-        PSeq a b -> go a >> go b
-        PAnn a o -> do
-          elt <- createElement htmlDoc "span" HTMLElement :: IO HTMLElement
-          E.setAttribute elt "class" (toClass a)
-          render htmlDoc (elt :: HTMLElement) toClass o
-          appendChild parent (Just elt)
-
---------------------
-
-
 
 
 ----------------------
 
 data LispAnn = LSym | LKwd | LExpr | LOpen | LClose
   deriving (Eq, Ord, Show)
-
-newtype RenderM a = RenderM { runRender :: (Document, HTMLElement) -> IO a }
-  deriving (Functor)
-
-instance Applicative RenderM where
-  pure = RenderM . const . pure
-  RenderM f <*> RenderM x =
-    RenderM (\doc -> do
-                g <- f doc
-                y <- x doc
-                pure $ g y)
-
-instance Monad RenderM where
-  return = pure
-  (RenderM x) >>= f = RenderM (\doc -> do
-                                  y <- x doc
-                                  runRender (f y) doc)
-
-instance MonadIO RenderM where
-  liftIO act = RenderM (const act)
-
-askDoc :: RenderM Document
-askDoc = RenderM  (pure . fst)
-
-askParent :: RenderM HTMLElement
-askParent = RenderM (pure . snd)
-
-chunkWidths :: IORef (M.Map (Chunk Double, [String]) Double)
-chunkWidths = unsafePerformIO (newIORef M.empty) -- TODO: move into RenderM state
-
-measureChunk :: (Chunk Double, [String]) -> RenderM Double
-measureChunk ch@(CText (preProcess -> txt), fmt) = do
-  found <- M.lookup ch <$> liftIO (readIORef chunkWidths)
-  case found of
-    Just r -> return r
-    Nothing -> do
-      doc    <- askDoc
-      parent <- askParent
-
-      elt <- createElement doc "span" HTMLElement
-      txtNode <- createTextNodeUnsafe' doc (T.unpack txt)
-      appendChild elt (Just txtNode)
-      E.setAttribute elt "className" (concat (intersperse " " fmt))
-      E.setAttribute elt "display" "hidden"
-      appendChild parent (Just elt)
-      liftIO $ threadDelay 1
-      w <- E.getOffsetWidth elt
-      removeChild parent (Just elt)
-
-      liftIO $ atomicModifyIORef' chunkWidths $ \ old -> (M.insert ch w old, ())
-      return w
-measureChunk (CSpace w, _)  = pure w
-
-newtype DocM a = DocM { unDocM :: RWST (PEnv Double LispAnn [String])
-                                       (POut Double LispAnn)
-                                       (PState Double [String])
-                                       (MaybeT RenderM)
-                                        a }
-  deriving
-    ( Functor, Applicative, Monad, Alternative
-    , MonadReader (PEnv Double LispAnn [String])
-    , MonadWriter (POut Double LispAnn)
-    , MonadState (PState Double [String])
-    )
-
-instance MonadPretty Double LispAnn [String] DocM
-
-runDocM :: PEnv Double LispAnn [String] -> PState Double [String] -> DocM a
-        -> RenderM (Maybe (PState Double [String], POut Double LispAnn, a))
-runDocM e s d = fmap rearrange <$> (runMaybeT (runRWST (unDocM d) e s))
-  where rearrange (a, s', o) = (s', o, a)
-
-
-instance Measure Double [String] DocM where
-  measure l = sum <$> mapM (liftRender . measureChunk) l
-
-liftRender :: RenderM a -> DocM a
-liftRender act = DocM (lift (lift act))
 
 lispKeywords = map T.pack ["lambda", "λ", "cond"]
 
@@ -238,7 +115,7 @@ prettyR (ImproperList xs) =
         withDot [x, y] = [x, char '.', y]
         withDot (x:y:zs) = x : withDot (y:zs)
 
-d1, d2, d3, d4, d5 :: DocM ()
+d1, d2, d3, d4, d5 :: DocM LispAnn ()
 d1 = prettyR (toR e1)
 d2 = prettyR (toR e2)
 d3 = prettyR (toR e3)
@@ -246,126 +123,24 @@ d4 = prettyR (toR e4)
 d5 = prettyR (toR e5)
 --------------------
 
-
---------------------
-state0 :: PState Double [String]
-state0 = PState
-  { curLine = []
-  }
-
-env0 :: Monoid fmt => Double ->  PEnv Double a fmt
-env0 w = PEnv
-  { maxWidth = w
-  , maxRibbon = (w * 4) / 5
-  , layout = Break
-  , failure = CantFail
-  , nesting = 0
-  , formatting = mempty
-  , formatAnn = const mempty
-  }
-
-
-execDoc :: (MonadIO m) => Document -> HTMLElement -> DocM () -> m (POut Double LispAnn)
-execDoc htmlDoc parent d = do
-  parentWidth <- E.getOffsetWidth parent
-  foo <- liftIO $ runRender (runDocM (env0 parentWidth) state0 d) (htmlDoc, parent)
-  case foo of
-    Nothing -> pure $ PAtom $ AChunk $ CText $ T.pack "<internal pretty printing error>"
-    Just (_, o, ()) -> pure o
-
 --------------------
 createElement doc tagName tagType =
   createElementUnsafe doc (Just tagName) >>= unsafeCastTo tagType
 
-record :: Document -> HTMLElement -> String -> EventM e t ()
-record doc parent line = do
-  case parse (phrase readExpr) line of
-    Left err -> liftIO $ putStrLn err
-    Right (_, e) ->
-      do let d = prettyR (toR e)
-         o <- liftIO $ execDoc doc parent d
-         liftIO $ render doc parent (const "") o
-         br <- createElement doc "br" HTMLBRElement
-         appendChild parent (Just br)
-         liftIO $ threadDelay 1
-         return ()
-
+config :: REPLConfig LispAnn
+config =
+  REPLConfig
+    { handleInput = hI
+    , showAnn = show
+    }
+  where hI line =
+          case parse (phrase readExpr) line of
+            Left err -> Left err
+            Right (_, e) ->
+              Right (prettyR (toR e))
 
 main :: IO ()
-main = do
-  Just doc <- currentDocument
-  Just window <- currentWindow
-  Just body <- getBody doc
+main = jsREPL config
 
-  transcript <- createElement doc "div" HTMLElement
-  input      <- createElement doc "input" HTMLInputElement
-  button     <- createElement doc "input" HTMLInputElement
-  setType button "button"
-  setValue button (Just "Go")
-
-  -- examples <- createElement doc "select" HTMLElement
-  -- for_ [e1, e2, e3, e4, e5] $ \e ->
-  --   opt <- createElement doc "option" HTMLElement
-    
-
-  outputRecord <- newIORef [] :: IO (IORef [DocM ()])
-
-  appendChild body (Just transcript)
-  appendChild body (Just input)
-  appendChild body (Just button)
-  threadDelay 1
-
-  renderLock <- newMVar ()
-
-  let refreshOut = forkIO $ do
-        ok <- tryTakeMVar renderLock
-        case ok of
-          Just () -> do
-            E.setInnerHTML transcript (Just "")
-            out <- reverse <$> readIORef outputRecord
-            for_ out $ \ d -> do
-              o <- execDoc doc transcript d
-              render doc transcript show o
-              threadDelay 1 -- render a browser frame
-              br <- createElement doc "br" HTMLBRElement
-              appendChild transcript (Just br)
-            putMVar renderLock ()
-          Nothing -> pure ()
-
-  let addOut e = forkIO $
-        atomicModifyIORef' outputRecord (\ old -> (prettyR (toR e) : old, ()))
-
-
-  let save line =
-       case parse (phrase readExpr) line of
-         Left err -> liftIO $ do
-           w <- currentWindow
-           case w of
-             Just win -> liftIO $ alert win err
-             Nothing -> liftIO $ putStrLn err
-         Right (_, e) ->
-           liftIO $ do
-             addOut e
-             refreshOut
-             setValue input (Just "")
-             return ()
-
-  let userInput = getValueUnsafe input :: EventM e t String
-
-  on button E.click (userInput >>= save)
-  on input E.keyPress $ do
-    wh <- uiWhich
-    if wh == 13
-      then (userInput >>= save)
-      else return ()
-
-  (Just w) <- currentWindow
-  on w resize $ liftIO (refreshOut *> pure ())
-
-
-  liftIO $ return ()
-
-createTextNodeUnsafe' :: (MonadIO m, IsDocument self) => self -> String -> m Text
-createTextNodeUnsafe' = createTextNodeUnsafe
 
 
--- jsrepl/src/Lib.hs
+++ jsrepl/src/Lib.hs
@@ -1,14 +1,284 @@
-{-# LANGUAGE OverloadedStrings #-}
 
-module Lib
-    ( console_log
-    , someFunc
-    ) where
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ViewPatterns #-}
 
-import Data.JSString ()
-import GHCJS.Types
+module Lib ( DocM(..)
+           , REPLConfig(..)
+           , jsREPL
+           ) where
 
-foreign import javascript unsafe "console.log($1)" console_log :: JSString -> IO ()
+import Data.IORef (IORef(..), newIORef, readIORef, atomicModifyIORef')
+import Control.Applicative
+import Control.Monad.Identity
+import Control.Monad.Trans.Maybe
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State
+import Control.Monad.RWS
 
-someFunc :: IO ()
-someFunc = console_log "Hello from GHCJS!"
+import Control.Monad.IO.Class (MonadIO(..))
+
+import Control.Concurrent (threadDelay, forkIO)
+import Control.Concurrent.MVar (newMVar, tryTakeMVar, putMVar)
+import Data.Foldable (for_)
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+import Data.List (intersperse)
+
+import System.IO.Unsafe (unsafePerformIO) -- yolo
+
+import GHCJS.DOM (currentDocument, currentWindow)
+import GHCJS.DOM.Document (createElementUnsafe, createTextNodeUnsafe, getBody)
+import GHCJS.DOM.EventM (EventM, event, on, uiWhich)
+import GHCJS.DOM.HTMLInputElement (getValueUnsafe, setValue, setType)
+import GHCJS.DOM.Node (appendChild, removeChild)
+import GHCJS.DOM.Types ( Document
+                       , HTMLElement(..)
+                       , HTMLInputElement(..)
+                       , HTMLDivElement(..)
+                       , HTMLBRElement(..)
+                       , HTMLSpanElement(..)
+                       , IsDocument
+                       , IsNode
+                       , Text
+                       , unsafeCastTo
+                       )
+import GHCJS.DOM.Window (alert, resize)
+--import GHCJS.DOM.WindowTimers (setTimeout)
+
+import qualified GHCJS.DOM.Element as E (click, getAttribute, keyPress, setAttribute, load, getOffsetWidth, setInnerHTML)
+
+import Text.PrettyPrint.Final
+
+----------------------------------
+
+newtype RenderM a = RenderM { runRender :: (Document, HTMLElement) -> IO a }
+  deriving (Functor)
+
+instance Applicative RenderM where
+  pure = RenderM . const . pure
+  RenderM f <*> RenderM x =
+    RenderM (\doc -> do
+                g <- f doc
+                y <- x doc
+                pure $ g y)
+
+instance Monad RenderM where
+  return = pure
+  (RenderM x) >>= f = RenderM (\doc -> do
+                                  y <- x doc
+                                  runRender (f y) doc)
+
+-- Widths in HTML-land will be doubles representing pixels
+
+preProcess :: T.Text -> T.Text
+preProcess = T.replace (T.pack " ") (T.pack " ") -- it's a non breaking space char
+
+renderAtom :: (IsDocument d) => d -> Atom Double -> IO HTMLElement
+renderAtom htmlDoc ANewline = createElement htmlDoc "br" HTMLElement
+renderAtom htmlDoc (AChunk (CText (preProcess -> t))) = do
+  txt <- createTextNodeUnsafe' htmlDoc (T.unpack t)
+  elt <- createElement htmlDoc "span" HTMLElement
+  appendChild elt (Just txt)
+  return elt
+renderAtom htmlDoc (AChunk (CSpace w)) = do
+  elt <- createElement htmlDoc "span" HTMLElement
+  E.setAttribute elt "style" $
+    "display: inline-block;" ++
+    "min-width: " ++ show (round w) ++ "px;" ++
+    "max-width: " ++ show (round w) ++ "px;"
+  txt <- createTextNodeUnsafe' htmlDoc " "
+  appendChild elt (Just txt)
+  return elt
+
+
+render :: (IsDocument d) => d -> HTMLElement -> (ann -> String) -> POut Double ann -> IO ()
+render htmlDoc parent toClass out = go out >> return ()
+  where
+    go out =
+      case out of
+        PNull -> createTextNodeUnsafe' htmlDoc "" >>= appendChild parent . Just
+        PAtom a -> renderAtom htmlDoc a >>= appendChild parent . Just
+        PSeq a b -> go a >> go b
+        PAnn a o -> do
+          elt <- createElement htmlDoc "span" HTMLElement :: IO HTMLElement
+          E.setAttribute elt "class" (toClass a)
+          render htmlDoc (elt :: HTMLElement) toClass o
+          appendChild parent (Just elt)
+
+
+instance MonadIO RenderM where
+  liftIO act = RenderM (const act)
+
+askDoc :: RenderM Document
+askDoc = RenderM  (pure . fst)
+
+askParent :: RenderM HTMLElement
+askParent = RenderM (pure . snd)
+
+chunkWidths :: IORef (M.Map (Chunk Double, [String]) Double)
+chunkWidths = unsafePerformIO (newIORef M.empty) -- TODO: move into RenderM state
+
+measureChunk :: (Chunk Double, [String]) -> RenderM Double
+measureChunk ch@(CText (preProcess -> txt), fmt) = do
+  found <- M.lookup ch <$> liftIO (readIORef chunkWidths)
+  case found of
+    Just r -> return r
+    Nothing -> do
+      doc    <- askDoc
+      parent <- askParent
+
+      elt <- createElement doc "span" HTMLElement
+      txtNode <- createTextNodeUnsafe' doc (T.unpack txt)
+      appendChild elt (Just txtNode)
+      E.setAttribute elt "className" (concat (intersperse " " fmt))
+      E.setAttribute elt "display" "hidden"
+      appendChild parent (Just elt)
+      liftIO $ threadDelay 1
+      w <- E.getOffsetWidth elt
+      removeChild parent (Just elt)
+
+      liftIO $ atomicModifyIORef' chunkWidths $ \ old -> (M.insert ch w old, ())
+      return w
+measureChunk (CSpace w, _)  = pure w
+
+
+state0 :: PState Double [String]
+state0 = PState
+  { curLine = []
+  }
+
+env0 :: Monoid fmt => Double ->  PEnv Double a fmt
+env0 w = PEnv
+  { maxWidth = w
+  , maxRibbon = (w * 4) / 5
+  , layout = Break
+  , failure = CantFail
+  , nesting = 0
+  , formatting = mempty
+  , formatAnn = const mempty
+  }
+
+
+execDoc :: (MonadIO m) => Document -> HTMLElement -> DocM ann () -> m (POut Double ann)
+execDoc htmlDoc parent d = do
+  parentWidth <- E.getOffsetWidth parent
+  foo <- liftIO $ runRender (runDocM (env0 parentWidth) state0 d) (htmlDoc, parent)
+  case foo of
+    Nothing -> pure $ PAtom $ AChunk $ CText $ T.pack "<internal pretty printing error>"
+    Just (_, o, ()) -> pure o
+
+newtype DocM ann a = DocM { unDocM :: RWST (PEnv Double ann [String])
+                                           (POut Double ann)
+                                           (PState Double [String])
+                                           (MaybeT RenderM)
+                                           a }
+  deriving
+    ( Functor, Applicative, Monad, Alternative
+    , MonadReader (PEnv Double ann [String])
+    , MonadWriter (POut Double ann)
+    , MonadState (PState Double [String])
+    )
+
+instance MonadPretty Double ann [String] (DocM ann)
+
+runDocM :: PEnv Double ann [String] -> PState Double [String] -> DocM ann a
+        -> RenderM (Maybe (PState Double [String], POut Double ann, a))
+runDocM e s d = fmap rearrange <$> (runMaybeT (runRWST (unDocM d) e s))
+  where rearrange (a, s', o) = (s', o, a)
+
+
+instance Measure Double [String] (DocM ann) where
+  measure l = sum <$> mapM (liftRender . measureChunk) l
+
+liftRender :: RenderM a -> DocM ann a
+liftRender act = DocM (lift (lift act))
+
+
+-----------------------------------
+data REPLConfig ann =
+  REPLConfig
+    { handleInput :: String -> Either String (DocM ann ())
+    , showAnn     :: ann -> String}
+
+
+jsREPL :: REPLConfig ann -> IO ()
+jsREPL config = do
+  Just doc    <- currentDocument
+  Just window <- currentWindow
+  Just body   <- getBody doc
+
+  transcript <- createElement doc "div" HTMLElement
+  input      <- createElement doc "input" HTMLInputElement
+  button     <- createElement doc "input" HTMLInputElement
+
+  setType button "button"
+  setValue button (Just "Go")
+
+  outputRecord <- newIORef [] :: IO (IORef [pretty ()])
+
+  appendChild body (Just transcript)
+  appendChild body (Just input)
+  appendChild body (Just button)
+  threadDelay 1
+
+  renderLock <- newMVar ()
+
+  let refreshOut = forkIO $ do
+        ok <- tryTakeMVar renderLock
+        case ok of
+          Just () -> do
+            E.setInnerHTML transcript (Just "")
+            out <- reverse <$> readIORef outputRecord
+            for_ out $ \ d -> do
+              o <- execDoc doc transcript d
+              render doc transcript (showAnn config) o
+              threadDelay 1 -- render a browser frame
+              br <- createElement doc "br" HTMLBRElement
+              appendChild transcript (Just br)
+            putMVar renderLock ()
+          Nothing -> pure ()
+
+  let addOut doc = forkIO $
+        atomicModifyIORef' outputRecord (\ old -> (doc : old, ()))
+
+
+  let save line =
+        case handleInput config line of
+          Left err -> liftIO $ do
+            w <- currentWindow
+            case w of
+              Just win -> liftIO $ alert win err
+              Nothing -> liftIO $ putStrLn err
+          Right doc ->
+            liftIO $ do
+              addOut doc
+              refreshOut
+              setValue input (Just "")
+              return ()
+
+  let userInput = getValueUnsafe input :: EventM e t String
+
+  on button E.click (userInput >>= save)
+  on input E.keyPress $ do
+    wh <- uiWhich
+    if wh == 13
+      then (userInput >>= save)
+      else return ()
+
+  (Just w) <- currentWindow
+  on w resize $ liftIO (refreshOut *> pure ())
+
+
+  liftIO $ return ()
+
+
+createElement doc tagName tagType =
+  createElementUnsafe doc (Just tagName) >>= unsafeCastTo tagType
+
+createTextNodeUnsafe' :: (MonadIO m, IsDocument self) => self -> String -> m Text
+createTextNodeUnsafe' = createTextNodeUnsafe
